64324c1533452ac74ea5ffbbea98bba6ddf40083
Dsaw <jos.flores95@gmail.com>
2022-11-12 22:03:07 -0300

    [FIX] indentation

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View>
-      <Text>
-        PABLO PUTO
-      </Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>
+                primer commit
+            </Text>
+        </View>
+    )
 }

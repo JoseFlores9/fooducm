ee5235c4a37a80ccb575ffb361388472017a6eb9
Dsaw <jos.flores95@gmail.com>
2022-11-12 21:12:18 -0300

    [ADD] first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text>
+        PABLO PUTO
+      </Text>
+    </View>
+  )
+}

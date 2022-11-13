329099fdc7d4de04ff009af977e7dad9e6993e69
Dsaw <jos.flores95@gmail.com>
2022-11-13 01:40:51 -0300

    [ADD] buy button and logic to seleted products

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { listProducts } from './src/mocks/products'
 import Products from './src/screens/Products'
 
 export default function App() {
     return (
-        <Products />
+        <Products products={listProducts}/>
     )
 }
--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,29 +1,58 @@
-import React from 'react'
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import { listProducts } from '../mocks/products'
+import React, { useState } from 'react'
+import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 
-export default function Products() {
+export default function Products(props) {
+    const { products } = props
+    const [selectedProds, setSelectedProds] = useState([])
+
+    const handleSelectProds = (prodId) => {
+        let temProds = selectedProds.slice()
+        if(selectedProds.includes(prodId)){
+            temProds.splice(temProds.indexOf(prodId), 1)
+            setSelectedProds(temProds)
+        }
+        else{
+            temProds.push(prodId)
+            setSelectedProds(temProds)
+        }
+    }
+
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                {listProducts.map((p, index)=>
-                    <View key={index} style={styles.product}>
-                        <View style={{width: '100%'}}>
+        <View>
+            <ScrollView>
+                <View style={{backgroundColor: '#0000ff', padding: 8}}>
+                    <Text style={{fontSize: 30, color: '#fff'}}>
+                        Bebidas
+                    </Text>
+                    <Text style={{fontSize: 20, color: '#fff'}}>
+                        20 productos
+                    </Text>
+                </View>
+                <View style={styles.container}>
+                    {products.map((p, index)=>
+                        <View
+                        key={index}
+                        style={styles.product(index % 2 === 0, selectedProds.includes(p.productId))}
+                        >
                             <Image
                                 source={{uri: p.image}}
                                 style={styles.photo}
                                 />
-                            </View>
-                        <Text style={styles.title}>
-                            {p.title}
-                        </Text>
-                        <Text>
-                            {p.price}
-                        </Text>
-                    </View>
-                )}
-            </View>
-        </ScrollView>
+                            <Text style={styles.title}>
+                                {p.title}
+                            </Text>
+                            <Text>
+                                {p.price}
+                            </Text>
+                            <Button
+                                title='Comprar'
+                                onPress={() => handleSelectProds(p.productId)}
+                                />
+                        </View>
+                    )}
+                </View>
+            </ScrollView>
+        </View>
     )
 }
 
@@ -35,22 +64,26 @@ export const styles = StyleSheet.create({
     title: {
         fontSize: 20
     },
-    product: {
-        width: "50%",
+    product: (index, contains) => ({
+        width: "48%",
         borderRadius: 4,
-        borderColor: '#000',
-        borderWidth: 1,
-        flex: 0,
-        flexWrap: 'wrap',
-        alignItems: 'center' 
-    },
+        borderColor: '#0000ff',
+        borderWidth: contains ? 2 : 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: index ? 0 : 16,
+        marginBottom: 16,
+        paddingVertical: 16
+
+
+    }),
     container: {
         flex: 1,
         flexDirection: 'row',
         flexWrap: 'wrap',
         alignItems: 'flex-start',
-        padding: 4,
+        padding: 16,
     },
     price: {
     }
-}) 
+})
